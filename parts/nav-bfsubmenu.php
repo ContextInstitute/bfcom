b0495a01f6bc3fac51e16cebf2433bb88f7835b6
Robert Gilman <rgilman@context.org>
2018-06-01 16:38:28 -0700

    Customize header with mainmenu and submenu
    Signed-off-by: Robert Gilman <rgilman@context.org>

---
+++ parts/nav-bfsubmenu.php
@@ -0,0 +1,21 @@
+<?php if ( bp_nouveau_has_nav() ) : ?>
+<nav class="main-navs no-ajax bp-navs single-screen-navs horizontal users-nav buddypress-wrap" id="object-nav" role="navigation" aria-label="User menu">
+	
+	<ul id="menu-subnav-1" class="medium-horizontal menu dropdown" >
+
+		<?php
+		while ( bp_nouveau_nav_items() ) :
+			bp_nouveau_nav_item();
+		?>
+
+			<li id="<?php bp_nouveau_nav_id(); ?>" class="<?php bp_nouveau_nav_classes(); ?>">
+				<a href="<?php bp_nouveau_nav_link(); ?>" id="<?php bp_nouveau_nav_link_id(); ?>">
+					<?php bp_nouveau_nav_link_text(); ?>
+				</a>
+			</li>
+
+		<?php endwhile; ?>
+
+	</ul>
+</nav>
+<?php endif; ?>
