bebc7fef472f841ec27b497f4ee41ad76b166560
Robert Gilman <rgilman@context.org>
2018-09-28 09:52:28 -0700

    Switched from dropdown-menu to dropdown-pane
    Also added new line at the end

--- parts/nav-bfc_top.php
+++ parts/nav-bfc_top.php
@@ -6,27 +6,24 @@
   <div class="header-topmenu">
 		<?php joints_top_nav(); ?>
 	</div>
-  <div class="header-user" style="z-index:100";>
-		<ul class="dropdown menu" data-dropdown-menu>
-			<li><a href="<?php bp_loggedin_user_link(); ?>"><?php bp_loggedin_user_avatar( 'type=full' ); ?></a>
-				<!-- check buddypress.wp-a2z.org for details on these functions. The parameters for ..._avatar are under bp_core_fetch_avatar() -->
-				<ul class="menu">
-					<li><a href="/wp-admin/">Admin Dashboard</a></li>
-					<li><a href="/wp-login.php?action=logout">Logout</a></li>
-					<!-- logout doesn't fully work yet -->
-					<li><a href="/members/<?php bp_loggedin_user_username(); ?>/profile/edit/">Edit My Profile</a></li>
-					<li><a href="/members/<?php bp_loggedin_user_username(); ?>/profile/change-avatar/">Change Avatar</a></li>
-					<li><a href="/members/<?php bp_loggedin_user_username(); ?>/messages/">Messages</a></li>
-					<li><a href="/members/<?php bp_loggedin_user_username(); ?>/compose/">Compose Message</a></li>
-					<li><a href="/members/<?php bp_loggedin_user_username(); ?>/settings/">Email/Password Settings</a></li>
-					<li><a href="/members/<?php bp_loggedin_user_username(); ?>/settings/notifications/">Notification Settings</a></li>
-					<li><a href="/members/<?php bp_loggedin_user_username(); ?>/settings/profile/">Profile Settings</a></li>
-				</ul>
-			</li>
-		</ul>
-		
+  <div class="header-user">
+		<a href="<?php bp_loggedin_user_link(); ?>" data-toggle="user-dropdown"><?php bp_loggedin_user_avatar( 'type=full' ); ?>
+		<div class="dropdown-pane" id="user-dropdown" data-dropdown data-hover="true" data-hover-pane="true" data-auto-focus="true">
+			<ul>
+				<li><a href="/wp-admin/">Admin Dashboard</a></li>
+				<li><a href="/wp-login.php?action=logout">Logout</a></li>
+				<!-- logout doesn't fully work yet -->
+				<li><a href="/members/<?php bp_loggedin_user_username(); ?>/profile/edit/">Edit My Profile</a></li>
+				<li><a href="/members/<?php bp_loggedin_user_username(); ?>/profile/change-avatar/">Change Avatar</a></li>
+				<li><a href="/members/<?php bp_loggedin_user_username(); ?>/messages/">Messages</a></li>
+				<li><a href="/members/<?php bp_loggedin_user_username(); ?>/compose/">Compose Message</a></li>
+				<li><a href="/members/<?php bp_loggedin_user_username(); ?>/settings/">Email/Password Settings</a></li>
+				<li><a href="/members/<?php bp_loggedin_user_username(); ?>/settings/notifications/">Notification Settings</a></li>
+				<li><a href="/members/<?php bp_loggedin_user_username(); ?>/settings/profile/">Profile Settings</a></li>
+			</ul>
+		</div>
 	</div>
-    <div class="header-submenu">
+  <div class="header-submenu">
 		<?php get_template_part( 'parts/nav', 'bfc_submenu' ); ?>
 	</div>
 </header>
