4e540d07fe56d330ecb50aac8fd396e49e114397
Robert Gilman <rgilman@context.org>
2018-10-01 09:36:20 -0700

    Remove image, banner, submenu from forum header

--- buddypress/groups/single/home.php
+++ buddypress/groups/single/home.php
@@ -13,19 +13,9 @@ if ( bp_has_groups() ) :
 
 		<?php bp_nouveau_group_hook( 'before', 'home_content' ); ?>
 
-		<div id="item-header" role="complementary" data-bp-item-id="<?php bp_group_id(); ?>" data-bp-item-component="groups" class="groups-header single-headers">
-
-			<?php bp_nouveau_group_header_template_part(); ?>
-
-		</div><!-- #item-header -->
 
 		<div class="bp-wrap">
 
-			<?php if ( ! bp_nouveau_is_object_nav_in_sidebar() ) : ?>
-
-				<?php bp_get_template_part( 'groups/single/parts/item-nav' ); ?>
-
-			<?php endif; ?>
 
 			<div id="item-body" class="item-body">
 
