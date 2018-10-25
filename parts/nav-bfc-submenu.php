01ee2f4ba50d9279e6f30c4eca6cf869fd008f4e
Robert Gilman <rgilman@context.org>
2018-10-24 20:21:16 -0700

    Added second copy of submenu for More… dropdown
    The changes through line 5 are all just formatting.
    
    Lines 18-34 output a second copy of the submenu to fill the dropdown panel.
    
    #more and .menu-more are new and don’t have any CSS attach to them yet.
    
    The dropdown-related items on lines 19 and 20 are taken from the user dropdown.
    
    The id’s and classes on lines 26 and 27 are taken from lines 10 and 11. This double usage may need to be revised. At the moment, it’s just in as a placeholder/prototype.

--- parts/nav-bfc-submenu.php
+++ parts/nav-bfc-submenu.php
@@ -1,8 +1,7 @@
 <?php if ( bfc_nouveau_has_nav() ) : ?>
-<nav class="main-navs no-ajax bp-navs single-screen-navs horizontal users-nav buddypress-wrap" id="object-nav" role="navigation" aria-label="User menu">
-	
-	<ul id="menu-subnav-1" class="medium-horizontal menu dropdown" >
+<nav class="main-navs no-ajax bp-navs single-screen-navs horizontal users-nav buddypress-wrap" id="object-nav" role="navigation" aria-label="User menu"> 	
 
+	<ul id="menu-subnav-1" class="medium-horizontal menu dropdown" >
 		<?php
 		while ( bp_nouveau_nav_items() ) :
 			bp_nouveau_nav_item();
@@ -16,6 +15,23 @@
 
 		<?php endwhile; ?>
 
+		<li id="more" class="<?php bp_nouveau_nav_classes(); ?>">
+		<span class="menu-more" data-toggle="more-dropdown">More ...</span>
+			<div class="dropdown-pane" id="more-dropdown" data-dropdown data-hover="true" data-hover-pane="true" data-auto-focus="true">
+				<ul>
+					<?php
+					while ( bp_nouveau_nav_items() ) :
+						bp_nouveau_nav_item();
+					?>
+						<li id="<?php bp_nouveau_nav_id(); ?>" class="<?php bp_nouveau_nav_classes(); ?>">
+							<a href="<?php bp_nouveau_nav_link(); ?>" id="<?php bp_nouveau_nav_link_id(); ?>">
+								<?php bp_nouveau_nav_link_text(); ?>
+							</a>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+			</div>
+		</li>
 	</ul>
 </nav>
 <?php endif; 
