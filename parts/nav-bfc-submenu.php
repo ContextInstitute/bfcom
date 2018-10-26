7c388a6175a48178eb666e4a33eaa1663562c9ab
Robert Gilman <rgilman@context.org>
2018-10-25 21:44:48 -0700

    Classes to support responsive submenu

--- parts/nav-bfc-submenu.php
+++ parts/nav-bfc-submenu.php
@@ -1,5 +1,15 @@
 <?php if ( bfc_nouveau_has_nav() ) : ?>
-<nav class="main-navs no-ajax bp-navs single-screen-navs horizontal users-nav buddypress-wrap" id="object-nav" role="navigation" aria-label="User menu"> 	
+
+<?php
+$nav_class = "main-navs no-ajax bp-navs single-screen-navs horizontal users-nav buddypress-wrap";
+if (bp_is_user()) {
+	$nav_class .= " bp-personal-nav";
+} elseif (bp_is_group()) {
+	$nav_class .= " bp-group-nav";
+}
+?>
+
+<nav class="<?php echo $nav_class; ?>" id="object-nav" role="navigation" aria-label="User menu"> 	
 
 	<ul id="menu-subnav-1" class="medium-horizontal menu dropdown" >
 		<?php
@@ -14,11 +24,12 @@
 			</li>
 
 		<?php endwhile; ?>
-
+	</ul>
+	<ul id="menu-subnav-2" class="medium-horizontal menu dropdown" >
 		<li id="more" class="<?php bp_nouveau_nav_classes(); ?>">
 		<span class="menu-more" data-toggle="more-dropdown">More ...</span>
-			<div class="dropdown-pane" id="more-dropdown" data-dropdown data-hover="true" data-hover-pane="true" data-auto-focus="true">
-				<ul>
+			<div class="dropdown-pane" id="more-dropdown" data-dropdown data-hover="true" data-hover-pane="true" data-auto-focus="false">
+				<ul class="more-ul">
 					<?php
 					while ( bp_nouveau_nav_items() ) :
 						bp_nouveau_nav_item();
