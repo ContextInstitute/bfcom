465b4602cc5b48c46036abc16c125e85372b5716
Robert Gilman <rgilman@context.org>
2018-11-22 08:21:13 -0800

    Customize group home page with widget areas

--- buddypress/groups/single/default-front.php
+++ buddypress/groups/single/default-front.php
@@ -9,27 +9,6 @@
 
 <div class="group-front-page">
 
-	<?php if ( ! is_active_sidebar( 'sidebar-buddypress-groups' ) || ! bp_nouveau_groups_do_group_boxes() ) : ?>
-		<?php if ( ! is_customize_preview() && bp_current_user_can( 'bp_moderate' ) ) : ?>
-
-			<div class="bp-feedback custom-homepage-info info no-icon">
-				<strong><?php esc_html_e( 'Manage the Groups default front page', 'buddypress' ); ?></strong>
-
-				<p>
-				<?php
-				printf(
-					esc_html__( 'You can set your preferences for the %1$s or add %2$s to it.', 'buddypress' ),
-					bp_nouveau_groups_get_customizer_option_link(),
-					bp_nouveau_groups_get_customizer_widgets_link()
-				);
-				?>
-				</p>
-
-			</div>
-
-		<?php endif; ?>
-	<?php endif; ?>
-
 	<?php if ( bp_nouveau_groups_front_page_description() && bp_nouveau_group_has_meta( 'description' ) ) : ?>
 		<div class="group-description">
 
@@ -37,21 +16,35 @@
 
 		</div><!-- .group-description -->
 	<?php endif; ?>
-
-	<?php if ( bp_nouveau_groups_do_group_boxes() ) : ?>
-		<div class="bp-plugin-widgets">
-
-			<?php bp_custom_group_boxes(); ?>
-
-		</div><!-- .bp-plugin-widgets -->
+	<div id="bfc-dash-panels" class="bfc-dash-panels">
+	<?php if ( is_active_sidebar( 'dash_left_panel' ) ) : ?>
+		<div id="bfc-dash-panel-left" class="bfc-dash-panel-left widget-area">
+			<?php dynamic_sidebar( 'dash_left_panel' ); ?>
+		</div><!-- #bfc-dash-panel-left -->
 	<?php endif; ?>
 
-	<?php if ( is_active_sidebar( 'sidebar-buddypress-groups' ) ) : ?>
-		<div id="group-front-widgets" class="bp-sidebar bp-widget-area" role="complementary">
+	<?php if ( is_active_sidebar( 'dash_right_panel' ) ) : ?>
+		<div id="bfc-dash-panel-right" class="bfc-dash-panel-right widget-area">
+			<?php dynamic_sidebar( 'dash_right_panel' ); ?>
+		</div><!-- #bfc-dash-panel-right -->
+	<?php endif; ?>
+	</div>
 
-			<?php dynamic_sidebar( 'sidebar-buddypress-groups' ); ?>
-
-		</div><!-- .bp-sidebar.bp-widget-area -->
+	<div id="bfc-dash-accordion" class="bfc-dash-accordion accordion" data-accordion data-allow-all-closed="true">
+	<?php if ( is_active_sidebar( 'dash_left_panel' ) ) : ?>
+		<div id="bfc-dash-panel-top" class="bfc-dash-panel-top widget-area accordion-item" data-accordion-item>
+		<a href="#" class="accordion-title">Latest Updates</a>
+		<div class="accordion-content" data-tab-content>
+			<?php dynamic_sidebar( 'dash_left_panel' ); ?>
+		</div></div><!-- #bfc-dash-panel-top -->
 	<?php endif; ?>
 
+	<?php if ( is_active_sidebar( 'dash_right_panel' ) ) : ?>
+		<div id="bfc-dash-panel-bottom" class="bfc-dash-panel-bottom widget-area accordion-item" data-accordion-item>
+		<a href="#" class="accordion-title">Forum Posts</a>
+		<div class="accordion-content" data-tab-content>
+			<?php dynamic_sidebar( 'dash_right_panel' ); ?>
+		</div></div><!-- #bfc-dash-panel-bottom -->
+	<?php endif; ?>
+	</div>
 </div>
