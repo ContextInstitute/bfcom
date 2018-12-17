5aae7fb96599a131779b8d59a6907aea73cd6f54
Robert Gilman <rgilman@context.org>
2018-12-16 20:21:23 -0800

    Use .min version of jquery-migrate 3.0.1

--- functions/bfc-functions.php
+++ functions/bfc-functions.php
@@ -6,7 +6,7 @@ function replace_core_jquery_version() {
 	wp_deregister_script( 'jquery-core' );
 	wp_register_script( 'jquery-core', "https://code.jquery.com/jquery-3.3.1.min.js", array(), '3.3.1' );
 	wp_deregister_script( 'jquery-migrate' );
-	wp_register_script( 'jquery-migrate', "https://code.jquery.com/jquery-migrate-3.0.1.js", array(), '3.0.1' );
+	wp_register_script( 'jquery-migrate', "https://code.jquery.com/jquery-migrate-3.0.1.min.js", array(), '3.0.1' );
 }
 add_action( 'wp_enqueue_scripts', 'replace_core_jquery_version' );
 
